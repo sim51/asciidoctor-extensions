ee395667cccb15756064d488822b53044d8e56fb
Benoît Simard (@logisima) <contact@bsimard.com>
2017-09-19 15:24:42 +0200

    Remove Solr extension

--- extensions/solr-postprocessor.rb
+++
@@ -1,7 +0,0 @@
-RUBY_ENGINE == 'opal' ? (require 'solr-postprocessor/extension') : (require_relative 'solr-postprocessor/extension')
-
-Asciidoctor::Extensions.register do
-  if (@document.basebackend? 'html')
-    postprocessor SolrPostprocessor
-  end
-end
